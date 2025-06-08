73e77957e447601a5de7c02da1bf1962117fd806
shrinidhi Karanth <shrinidhiskaranth@gmail.com>
2025-06-09 02:28:58 +0530

    Added credentials to help login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("timefa6094@bankrau.com");
+  const [password, setPassword] = useState("abcd1234");
   const { login, isLoggingIn } = useLogin();
 
   function handleSubmit(e) {
